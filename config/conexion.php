dcc986c644684985f3053f9af4e13d77491805c2
Jhonier1227 <maily0877@gmail.com>
2025-03-10 12:03:49 -0500

    no hice nada

--- config/conexion.php
+++ config/conexion.php
@@ -9,12 +9,13 @@
 $host = "localhost";   // Servidor de la base de datos
 $usuario = "root";     // Usuarios de MySQL(por defecto en XAMPP)
 $password = "";        // Contraseña(vacia por defecto en XAMPP)
-$base_datos = "Gestor";  // Nombre de base de datos.
+$base_datos = "DoltNow";  // Nombre de base de datos.
 
 try {
     // Se establece la conexión a la base de datos.
 
     $pdo = new PDO("mysql:host=$host;dbname=$base_datos;charset=utf8", $usuario, $password);
+    echo "conexion exitosa";
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     // Activar errores de POO.
 } catch (PDOException $e) {
